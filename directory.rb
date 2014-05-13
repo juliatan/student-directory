f80d63a8ffd4ee8b06f8cc05ef8a6500bf6aa2a8
juliatan <hello@julia-tan.com>
2014-05-13 11:57:01 +0100

    Added full array and used .length method

--- directory.rb
+++ directory.rb
@@ -1,12 +1,38 @@
-student_count = 9
+# let's put all students into an array
+students = [
+	"Roi Driscoll",
+	"James Whyte",
+	"Margherita Serino",
+	"Vaidas Mykolaitis",
+	"Johann Bornman",
+	"Kate Hamilton",
+	"James Keap",
+	"Nic Yeeles",
+	"Julie Walker",
+	"Will Allen",
+	"Julia Tan",
+	"Federico Maffei",
+	"Jamie Patel",
+	"Faezrah Rizalman",
+	"Josh Fail-Broon",
+	"Sasha Cooper",
+	"Nicolai DTH",
+	"Nadav Matalon",
+	"Fitsum Teklehaimanot",
+]
 
-# first we print the list of students
-
+# and then prints them
 puts "The students of my cohort at Makers Academy"
 puts "-----------------------------"
-puts "Federico Maffei"
-puts "Julia Tan"
-puts "blah blah"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
 
 # finally we print the total
-print "Overall, we have #{student_count} great students"
+print "Overall, we have #{student.length} great students"
