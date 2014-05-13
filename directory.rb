2762c7e108a73c910a4ad00b11850109ad3e9587
juliatan <hello@julia-tan.com>
2014-05-13 11:20:10 +0100

    Added total student count and comments

--- directory.rb
+++ directory.rb
@@ -1,5 +1,15 @@
+# first we print the list of students
+
 puts "The students of my cohort at Makers Academy"
 puts "-----------------------------"
 puts "Federico Maffei"
 puts "Julia Tan"
-puts ""
+puts "blah blah"
+
+# finally we print the total
+print "Overall, we have "
+# it's important that print() doesn't add new line characters
+print 9
+puts " great students"
+
+# print "line1\nline2\nline3" # has to be in double quotes
