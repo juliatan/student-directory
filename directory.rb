e296824e379a447cf458c45f524cbd0721f9a739
juliatan <hello@julia-tan.com>
2014-05-13 12:03:59 +0100

    Added iterator code to automatically puts each member of the students array

--- directory.rb
+++ directory.rb
@@ -18,21 +18,15 @@ students = [
 	"Sasha Cooper",
 	"Nicolai DTH",
 	"Nadav Matalon",
-	"Fitsum Teklehaimanot",
+	"Fitsum Teklehaimanot"
 ]
 
 # and then prints them
 puts "The students of my cohort at Makers Academy"
 puts "-----------------------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
+students.each do |student|
+	puts student
+end
 
 # finally we print the total
 print "Overall, we have #{students.length} great students"
