51c2e56d7fd02fad2e78fabf114d89a43c119f99
juliatan <hello@julia-tan.com>
2014-05-14 14:20:54 +0100

    Added method for loading student file

--- directory.rb
+++ directory.rb
@@ -45,6 +45,8 @@ def process(selection)
 		show_students
 	when "3"
 		save_students
+	when "4"
+		load_students
 	when "9"
 		exit #this will cause the program to terminate
 	else
@@ -56,6 +58,7 @@ def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
 	puts "3. Save the list to students.csv"
+	puts "4. Load the list from students.csv"
 	puts "9. Exit"
 end
 
@@ -99,7 +102,7 @@ end
 
 def save_students
 	# open the file for writing
-	file = File.open("students.csv", "a+")
+	file = File.open("students.csv", "w")
 	# iterate over the array of students
 	@students.each do |student|
 		student_data = [student[:name], student[:cohort]]
@@ -107,6 +110,16 @@ def save_students
 		file.puts csv_line
 	end
 	file.close
+end
+
+def load_students
+	file = File.open("students.csv", "r")
+	file.readlines.each do |line|
+		name, cohort = line.chomp.split(",")
+		@students << {name: name, cohort: cohort.to_sym}
+	end
+	file.close
+	puts "It's been loaded"
 end
 
 interactive_menu
