30b82ffe54c9064951e9903666382fad0956260c
juliatan <hello@julia-tan.com>
2014-05-14 11:51:13 +0100

    Defined more succint methods

--- directory.rb
+++ directory.rb
@@ -22,43 +22,72 @@ students = [
 	{name: "Fitsum Teklehaimanot", cohort: :may}
 ]
 =end
+@students = []
 
-# and then prints them
+def interactive_menu
+	loop do
+		# 1. print the menu and ask the user what to do
+		print_menu
+		# 2. read the input and save it to a variable
+		input = gets.chomp
 
-# code to get user input
+		# 3. do what the user has asked
+		case input
+		when "1"
+			 input_students 
+		when "2"
+			show_students
+		when "9"
+			exit #this will cause the program to terminate
+		else
+			puts "I don't know what you meant, try again"
+		end
+
+	end
+end
+	# 4. repeat from step 1
+
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+end
+
+def show_students
+	print_header
+	print
+	print_footer
+end
+
+
+# Option 1
 def input_students
 	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice"
-	students = []
 	name = gets.chomp
 	
 	while !name.empty? do 
-		students << {name: name, cohort: :may}
-		puts "Now we have #{students.length} students"
+		@students << {name: name, cohort: :may}
+		puts "Now we have #{@students.length} students"
 		name = gets.chomp
 	end
-
-	# return the array of students
-	students
 end
+
+# Option 2
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "-----------------------------"
 end
 
-def print(students)
-		students.each do |student|
+def print
+		@students.each do |student|
 		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
 
-def print_footer(names)
-	puts "Overall, we have #{names.length} great students"
+def print_footer
+	puts "Overall, we have #{@students.length} great students"
 end
 
-# nothing happens until we call the methods
-students = input_students 
-print_header
-print(students)
-print_footer(students)
+interactive_menu
